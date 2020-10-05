63e97816f5b1c7d9402e11ede2e1a279aee27f49
Yanina Likhtenshteyn <ylikhten@mymail.mines.edu>
2020-10-04 22:06:31 -0600

    fixed some javascript issues in the alphabet-learn view; started implementing mongodb. Finished up to section 5.3 in textbook

--- app_server/controllers/lessons.js
+++ app_server/controllers/lessons.js
@@ -1,11 +1,37 @@
 /* GET home page */
 module.exports.home = function(req, res){
-	res.render('index', {title: 'Alpabes Home'});
+	res.render('index', {
+		title: 'Alpabes--Hangul Practice',
+		pageHeader: {
+			title: 'Alpabes',
+			strapline: 'Learn and practice Hangul'
+		}
+	
+	});
 };
 
 /* GET 'Learn' page */
 module.exports.learn = function(req, res){
-	res.render('alphabet-list', {title: 'Learn Hangul'});
+	res.render('alphabet-learn', {
+		title: 'Learn Hangul',
+		characters: [{
+			name: 'Bieup',
+			pronunciation: 'b/p',
+			example: 'butter',
+			src: 'images/Korean_Alphabet_bieup.png'
+		},{
+			name: 'Giyeok',
+			pronunciation: 'g/k',
+			example: 'duck',
+			src: 'images/Korean_Alphabet_giyeok.png'
+		},{
+			name: 'Mieum',
+			pronunciation: 'ma',
+			example: 'material',
+			src: 'images/Korean_Alphabet_mieum.png'
+		}]	
+		
+	});
 };
 
 /* GET 'Practice' page */
