353307cd7280a2332aefbfbe884a2606327fb133
Grace <kangrace@yahoo.com>
2020-10-08 00:56:52 -0600

    Minor aesthetic changes
    Added more letters and tweaked the look of the page.

--- app_server/controllers/lessons.js
+++ app_server/controllers/lessons.js
@@ -15,20 +15,35 @@ module.exports.learn = function(req, res){
 	res.render('alphabet-learn', {
 		title: 'Learn Hangul',
 		characters: [{
-			name: 'Bieup',
-			pronunciation: 'b/p',
-			example: 'butter',
-			src: 'images/Korean_Alphabet_bieup.png'
-		},{
 			name: 'Giyeok',
 			pronunciation: 'g/k',
 			example: 'duck',
 			src: 'images/Korean_Alphabet_giyeok.png'
 		},{
+			name: 'Nieun',
+			pronunciation: 'n',
+			example: 'neither',
+			src: 'images/Korean_Alphabet_nieun.png'
+		},{
+			name: 'Digeut',
+			pronunciation: 'd',
+			example: 'dot',
+			src: 'images/Korean_Alphabet_digeut.png'
+		},{
+			name: 'Rieul',
+			pronunciation: 'r/l',
+			example: 'real',
+			src: 'images/Korean_Alphabet_rieul.png'
+		},{
 			name: 'Mieum',
 			pronunciation: 'ma',
 			example: 'material',
 			src: 'images/Korean_Alphabet_mieum.png'
+		},{
+			name: 'Bieup',
+			pronunciation: 'b/p',
+			example: 'butter',
+			src: 'images/Korean_Alphabet_bieup.png'
 		}]	
 		
 	});
