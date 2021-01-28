d94fdb746a6bcf43d36c4c87d8ba0fca6c980660
ylikhten <ylikhten@mymail.mines.edu>
2021-01-27 18:27:45 -0700

    Mostly cosmetic changes to the practice page. Also fixed copyright to bottem left of container.

--- app_server/controllers/lessons.js
+++ app_server/controllers/lessons.js
@@ -52,5 +52,36 @@ module.exports.learn = function(req, res){
 
 /* GET 'Practice' page */
 module.exports.practice = function(req, res){
-	res.render('alphabet-practice', {title: 'Practice Hangul here', menu: 'Practice'});
+	res.render('alphabet-practice', {title: 'Practice Hangul here', menu: 'Practice', 
+	characters: [{
+			name: 'Giyeok',
+			pronunciation: 'g/k',
+			example: 'duck',
+			src: 'images/Korean_Alphabet_giyeok.png'
+		},{
+			name: 'Nieun',
+			pronunciation: 'n',
+			example: 'neither',
+			src: 'images/Korean_Alphabet_nieun.png'
+		},{
+			name: 'Digeut',
+			pronunciation: 'd',
+			example: 'dot',
+			src: 'images/Korean_Alphabet_digeut.png'
+		},{
+			name: 'Rieul',
+			pronunciation: 'r/l',
+			example: 'real',
+			src: 'images/Korean_Alphabet_rieul.png'
+		},{
+			name: 'Mieum',
+			pronunciation: 'ma',
+			example: 'material',
+			src: 'images/Korean_Alphabet_mieum.png'
+		},{
+			name: 'Bieup',
+			pronunciation: 'b/p',
+			example: 'butter',
+			src: 'images/Korean_Alphabet_bieup.png'
+		}]	});
 };
