a0865861cf5fba5b49d03f8421c1b9d66ccf4106
ylikhten <ylikhten@mymail.mines.edu>
2021-02-06 12:25:27 -0700

    The api GETs successfully on localhost! Now we have to populate the db. Got up through 7.2.3 in textbook

--- app_server/controllers/lessons.js
+++ app_server/controllers/lessons.js
@@ -1,4 +1,10 @@
 var request = require('request');
+var apiOptions = {
+  server: 'http://localhost:3000'
+};
+if (process.env.NODE_ENV === 'production'){
+  apiOptions.server = 'https://fathomless-ridge-97112.herokuapp.com'
+}
 
 /* GET home page */
 module.exports.home = function(req, res){
@@ -8,51 +14,32 @@ module.exports.home = function(req, res){
 			title: 'Alpabes',
 			strapline: 'Learn and practice Hangul'
 		}
-	
 	});
 };
 
-var renderLearnPage = function(req, res){
+var renderLearnPage = function(req, res, body){
   res.render('alphabet-learn', {
     title: 'Learn Hangul',
 		menu: 'Learn',
-		characters: [{
-			name: 'Giyeok',
-			pronunciation: 'g/k',
-			example: 'duck',
-			src: 'images/Korean_Alphabet_giyeok.png'
-		},{
-			name: 'Nieun',
-			pronunciation: 'n',
-			example: 'neither',
-			src: 'images/Korean_Alphabet_nieun.png'
-		},{
-			name: 'Digeut',
-			pronunciation: 'd',
-			example: 'dot',
-			src: 'images/Korean_Alphabet_digeut.png'
-		},{
-			name: 'Rieul',
-			pronunciation: 'r/l',
-			example: 'real',
-			src: 'images/Korean_Alphabet_rieul.png'
-		},{
-			name: 'Mieum',
-			pronunciation: 'ma',
-			example: 'material',
-			src: 'images/Korean_Alphabet_mieum.png'
-		},{
-			name: 'Bieup',
-			pronunciation: 'b/p',
-			example: 'butter',
-			src: 'images/Korean_Alphabet_bieup.png'
-		}]	
+		characters: body
   });
 };
 
 /* GET 'Learn' page */
 module.exports.learn = function(req, res){
-  renderLearnPage(req, res);
+  var requestOptions, path;
+  path = '/api/learn';
+  requestOptions = {
+    url: apiOptions.server + path,
+    method: "GET",
+    json: {},
+  };
+  request (
+    requestOptions,
+    function(err, response, body){
+      renderLearnPage(req, res, body);
+    }
+  );
 };
 
 /* GET 'Practice' page */
