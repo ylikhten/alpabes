99f95c610acbd49fe561a787148b1e9c359f3bc0
ylikhten <ylikhten@mymail.mines.edu>
2021-03-06 20:25:30 -0700

    added some comments

--- public/javascripts/postreq.js
+++ public/javascripts/postreq.js
@@ -1,7 +1,10 @@
 $(function () {
+  // refer to the class (.classname) instead of id (#idname) for 
+  // forms generated in a for loop.
+  // The id tag must be unique, so the below function will only apply 
+  // to the first object that got that tag
   $('.answer-form-class').submit(function(event) {
     var formid = this.id;
-    //window.alert(formid);
     event.preventDefault();
     event.stopPropagation();
     var dataToPost = {
@@ -9,44 +12,18 @@ $(function () {
       'charid': formid
     };
 
-    //window.alert(dataToPost.input);
     $.ajax({
       url: '/api/practice/' + $(formid),
       type: 'POST',
       contentType: 'application/json',
       data: JSON.stringify(dataToPost),
     }).done(function(data){
+      // set css class depending on answer from API
       if (data.correctAnswer) {
         $('#div-' + data.charid).removeClass('letters').addClass('correct'); 
       } else {
         $('#div-' + data.charid).removeClass('letters').addClass('incorrect'); 
       }
     });
-    
   });
-
 });
-/*
-+// SUBMIT FORM
-+$(function () {
-+  $('#test-form').submit(function(event) {
-+    //prevent form from submitting vie browser
-+    // i.e. stop page from reloading on submit
-+    event.preventDefault();
-+    event.stopPropagation();
-+    window.alert("pressed enter");
-+    var dataToPost = {
-+      'input': $('#test').val()
-+    };
-+    $.ajax({
-+      url: '/api/test',
-+      type: 'POST',
-+      contentType: 'application/json',
-+      data: JSON.stringify(dataToPost),
-+      success: function(result){
-+        window.alert("success " + result);
-+      }
-+    })
-+  });
-+});
-*/
