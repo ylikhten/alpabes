9ab7d9a9fbf2ddf37e2c308f62918ec52aa4ae01
ylikhten <ylikhten@mymail.mines.edu>
2021-03-21 22:12:21 -0600

    updated submit functionality on practice page. Tabbing after submit enabled

--- public/javascripts/postreq.js
+++ public/javascripts/postreq.js
@@ -9,6 +9,7 @@ $(function () {
     var formid = this.id;
     event.preventDefault();
     event.stopPropagation();
+
     var dataToPost = {
       'input': $('#answer-' + formid).val(),
       'charid': formid
@@ -20,12 +21,38 @@ $(function () {
       contentType: 'application/json',
       data: JSON.stringify(dataToPost),
     }).done(function(data){
+     
       // set css class depending on answer from API
       if (data.correctAnswer) {
         $('#div-' + data.charid).removeClass('letters').addClass('correct'); 
         // disable this input after correct answer submitted
-        $('#answer-' + data.charid).prop('disabled', true);;
-        $(this).next().trigger('focus');
+        $('#answer-' + data.charid).prop('disabled', true);
+
+
+        // focus on next available input
+        var inputs = $('input');
+        var count = 0;
+        var index = inputs.index($('#answer-' + formid));
+        while(index <= inputs.length) {
+          //set up testing for this part
+          
+          // if at end, check for earlier enabled inputs
+          //if(index === inputs.length - 1) {
+            
+          //}
+          // find next enabled input
+          if(inputs[index + count].disabled) {
+            count += 1;
+          } else {
+            index += count;
+            break;
+          }
+        }
+        inputs.eq(
+          inputs.length <= index ? 0 : index
+        ).focus();
+
+
       } else {
         $('#div-' + data.charid).removeClass('letters').addClass('incorrect'); 
         $('#answer-' + data.charid).addClass('incorrect-input');
